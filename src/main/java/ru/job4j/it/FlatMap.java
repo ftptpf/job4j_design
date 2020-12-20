62dc500ba1aaef62f65bf58ed6b9910e435df61d
ftptpf <Nb49Rtdd>
2020-12-20 20:14:30 +0300

    5.1.4. FlatMap для Iterator<Iterator> [#426135]

---
+++ src/main/java/ru/job4j/it/FlatMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.it;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlatMap {
+    public static void main(String[] args) {
+        List<List<Integer>> data = List.of(
+                List.of(1, 2, 3),
+                List.of(4, 5, 6),
+                List.of(7, 8, 9)
+        );
+        List<Integer> flat = data.stream().
+                flatMap(List::stream).
+                collect(Collectors.toList());
+        System.out.println(flat);
+    }
+}
---
+++ src/main/java/ru/job4j/it/FlatMapIter.java
@@ -0,0 +1,47 @@
+package ru.job4j.it;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class FlatMapIter<T> implements Iterator<T> {
+    private final Iterator<Iterator<T>> data; // вложенные итераторы
+    private Iterator<T> cursor = Collections.emptyIterator(); // текущий итератор
+
+    public FlatMapIter(Iterator<Iterator<T>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!cursor.hasNext() && data.hasNext()) {
+            cursor = data.next();
+        }
+        return cursor.hasNext();
+    }
+
+    /**
+     * Метод последовательно возвращает числа из вложенных мтераторов
+     * @return
+     */
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return cursor.next();
+    }
+
+    public static void main(String[] args) {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1, 2, 3).iterator(),
+                List.of(4, 5, 6).iterator(),
+                List.of(7, 8, 9).iterator()
+        ).iterator();
+        FlatMapIter<Integer> flat = new FlatMapIter<>(data);
+        while (flat.hasNext()) {
+            System.out.println(flat.next());
+        }
+    }
+}

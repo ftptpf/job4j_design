df43439c9dfb445a5c75f2225240905b8203b198
ftptpf <Nb49Rtdd>
2021-01-10 17:55:47 +0300

    3. Удалить head в односвязном списке. [#426124]

---
+++ src/main/java/ru/job4j/list/ForwardLinked.java
@@ -0,0 +1,102 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Организация односвязного списка.
+ * @param <T>
+ */
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head; // первый нод
+    private int modCount = 0; // счетчик изменений
+
+    /**
+     * Метод добавляет указанный элемент (value) в конец списка.
+     * @param value
+     */
+    public void add(T value) {
+        Node<T> node = new Node<>(value, null); // создаем новый нод
+        if (head == null) { // если первый нод (head) пустой
+            head = node; // инициализируем первый нод новым нодом
+            modCount++;
+            return;
+        }
+        Node<T> tail = head; // создаем новый нод
+        while (tail.next != null) { // пока у нового нода ссылка на следующий нод не станет равна null (не дошли до конца списка)
+            tail = tail.next; // идем по цепочке, переприсваивая ноды next значениями последующих нодов, доходим до конца списка
+        }
+        tail.next = node; // в предпоследнем элементе делаем ссылку на "новый" последний элемент
+        modCount++;
+    }
+
+    /**
+     * Метод удаляет первый элемент списка.
+     * @return
+     */
+    public T deleteFirst() {
+        Node<T> f = head;
+        if (f == null) {
+            throw new NoSuchElementException();
+        }
+        return unlinkFirst(f); // обнуление ссылки первого нода (head) на следующий за ним нод
+    }
+
+    /**
+     * Метод обнуляет значение и ссылку первого нода
+     * @param f
+     * @return
+     */
+    private T unlinkFirst(Node<T> f) {
+        final T element = f.value; // присваиваем значение первого нода
+        final Node<T> next = f.next; // next - второй нод (следующий после head)
+        f.value = null; // обнуляем значение первого нода head
+        f.next = null; // обнуляем ссылку первого нода head на следующий за ним нод
+        head = next; // инициализируем первый нод (head) вторым нодом (next), т.е. второй нод становится первым
+        modCount++;
+        return element;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private final int expectedModCount = modCount;
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    /**
+     * Односвязный список.
+     * @param <T>
+     */
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.next = next;
+            this.value = value;
+        }
+    }
+}

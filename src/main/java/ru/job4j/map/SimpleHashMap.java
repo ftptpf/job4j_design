ffb76f85e75e89a05864a2fee6893a1ef0d9aa45
ftptpf <inboxru@yandex.ru>
2021-02-05 09:40:18 +0300

    8. Реализовать собственную структуру данных - HashMap [#426162] - mode

--- src/main/java/ru/job4j/map/SimpleHashMap.java
+++ src/main/java/ru/job4j/map/SimpleHashMap.java
@@ -3,6 +3,7 @@ package ru.job4j.map;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 /**
  * Упрощенная реализация HashMap, динамического ассоциативного массива на базе хеш-таблицы.
@@ -14,9 +15,8 @@ import java.util.NoSuchElementException;
  * @param <V> значение
  */
 public class SimpleHashMap<K, V> implements Iterable<NodeMap<K, V>> {
-    // static final int DEFAULT_INITIAL_CAPACITY = 16; // константа начальной емкости массива
     static final float MAXIMUM_CAPACITY = 0.75f; // коэффициент загрузки массива
-    private int count; // количество добавленных в массив элементов
+    private int count; // колличество добавленных в массив элементов
     private NodeMap<K, V>[] array; // массив элементов (нодов)
     private int modCount = 0; // счетчик изменений
     private int arraySize = 16; // начальный размер массива
@@ -32,9 +32,10 @@ public class SimpleHashMap<K, V> implements Iterable<NodeMap<K, V>> {
      */
     public boolean insert(K key, V value) {
         int index = hashIndexPosition(key); // определяем индекс массива по которому будет производиться вставка
-        if (array[index] == null) { // если ячейка, к которой мы обратились, пуста
-            NodeMap<K, V> newNodeMap = new NodeMap<>(key, value); // создаем новыый нод
-            array[index] = newNodeMap; // добавляем его в ячеку массива
+        if (Objects.isNull(array[index])) { // если ячейка, к которой мы обратились, пуста
+/*        if (array[index].equals(null)) { // если ячейка, к которой мы обратились, пуста*/
+            NodeMap<K, V> newNodeMap = new NodeMap<>(key, value); // создаем новый нод
+            array[index] = newNodeMap; // добавляем его в ячейку массива
             modCount++; // увеличиваем счетчик произошедших с массивом изменений
             count++; // увеличиваем счетчик добавленных в массив нодов на единицу
             resize(); // вызываем метод проверки массива на заполнение и при необходимости увеличиваем размеры массива
@@ -50,43 +51,44 @@ public class SimpleHashMap<K, V> implements Iterable<NodeMap<K, V>> {
      * @return
      */
     private void resize() {
-        NodeMap<K, V>[] newNodeArray; // создаем объект нового расширенного массива
         if (count / arraySize >= MAXIMUM_CAPACITY) { // если превышено значение коэффициента загрузки массива
-            arraySize *= 2; // увеличиваем значение размера массива
-            newNodeArray = new NodeMap[arraySize]; // иничиализируем новый массив новым увеличенным размером
-            Iterator<NodeMap<K, V>> itOldArray = new SimpleHashMap<>();  // создаем итератор страрого массива
-            NodeMap<K, V> tempNode; // нод временных значений
-            int tempIndex = 0;
-/*            while (itOldArray.hasNext()) { // пока в массиве есть ноды
-                tempNode = itOldArray.next();
-                newNodeArray[itOldArray.next().]*/
-/*            while (array[tempIndex] != null) {
-                tempNode = array[tempIndex];*/
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] != null) {
-                    tempNode = array[i];
+            NodeMap<K, V>[] newNodeArray; // создаем объект нового расширенного массива
+            arraySize *= 2; // увеличиваем заначение размера массива
+            newNodeArray = new NodeMap[arraySize]; // инициализируем новый массив новым увеличенным размером
+            NodeMap<K, V>[] tempArray = array; // создаем клон старого массива
+            array = newNodeArray; // старому массиву присваиваем размер нового массива
+            int resizeCount = 0; // количество перенесенных в новый массив элементов
 
+            for (NodeMap<K,V> nod : tempArray) { // берем каждый нод из старого массива
+                if (Objects.nonNull(nod)) { // если нод не равен null
+                /*if (nod != null) { // если нод не равен null*/
+                    int hashIndex = hashIndexPosition(nod.getKey()); // высчитываем новый индекс по ключу нода
+                    if (Objects.nonNull(newNodeArray[hashIndex])) { // выполняем проверку содержит ли уже ячейка массива ранее записанный нод
+                    /*if (newNodeArray[hashIndex] != null) {*/
+                        newNodeArray[hashIndex] = nod; // если ячейка пустая, записывам нод в новый массив
+                        resizeCount++;
+                    } else {
+                        throw new AssertionError("Коллизия при расширении массива. Попытка вставить нод в заполненную ячейку");
+                        // System.out.println("Коллизия при расширении массива. Попытка вставить нод в заполненную ячейку");
+                    }
                 }
             }
-
-
-        }
-
-
             modCount++; // увеличиваем счетчик произошедших с массивом изменений
-            array = newNodeArray; // присваиваем значения нового массивая старому
-
+            System.out.println("В старом массиве было: " + count + " элементов.");
+            System.out.println("При расширении массива в новый перенесено: " + resizeCount + " элементов.");
+            count = resizeCount; // перезаписываем колличество добавленных элементов массива актуальным значением
+        }
     }
 
 
     /**
      * Метод метод возвращает значение по ключу.
      * @param key
-     * @return Если ячейка не содержит значения сопоставимого с ключем - возвращается null.
+     * @return Если ячейка не содержит значения сопоставимого с ключём - возвращается null.
      */
     public V get(K key) {
         int index = hashIndexPosition(key); // определяем индекс массива где должен располагаться нод
-        if (array[index] != null) { // проверяем не пуста ли ячека массива к которой мы обратились
+        if (!array[index].equals(null)) { // проверяем не пуста ли ячейка массива к которой мы обратились
             return array[index].getValue(); // возвращаем значение value нода, который хранится в ячейке массива
         }
         return null;
@@ -99,7 +101,7 @@ public class SimpleHashMap<K, V> implements Iterable<NodeMap<K, V>> {
      */
     public boolean delete(K key) {
         int index = hashIndexPosition(key); // определяем индекс массива по которому нам необходимо выполнить удаление
-        if (array[index] != null ) { // проверяем не пуста ли ячека массива к которой мы обратились
+        if (!array[index].equals(null)) { // проверяем не пуста ли ячейка массива к которой мы обратились
             array[index] = null; // обнуляем значения которые были записаны в ячейке
             modCount++; // увеличиваем счетчик произошедших с массивом изменений
             count--; // уменьшаем счетчик нодов, которые находятся в массиве
@@ -121,7 +123,7 @@ public class SimpleHashMap<K, V> implements Iterable<NodeMap<K, V>> {
     @Override
     public Iterator<NodeMap<K, V>> iterator() {
         return new Iterator<NodeMap<K, V>>() {
-            int cursor;
+            int index = 0; // текущий индекс в массиве
             int expectedModCount = modCount;
 
 
@@ -130,7 +132,16 @@ public class SimpleHashMap<K, V> implements Iterable<NodeMap<K, V>> {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                return cursor != arraySize;
+
+                while (index < array.length) { // пока индекс меньше размера массива
+                    if (Objects.nonNull(array[index])) {
+                    // if (array[index] == null); { // если значение ячейки массива равно null
+                        // index++;
+                        break;
+                    }
+                    index++; // увеличиваем индекс на единицу, чтобы перейти к проверке другой ячейки
+                }
+                return index < array.length;
             }
 
             @Override
@@ -140,56 +151,8 @@ public class SimpleHashMap<K, V> implements Iterable<NodeMap<K, V>> {
                 } else if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                return array[cursor];
+                return array[index];
             }
         };
     }
-
-
-
-/*    @Override
-    public Iterator<NodeMap<K, V>> iterator() {
-        return new Iterator<NodeMap<K, V>>() {
-            int cursor;
-            int expectedModCount = modCount;
-
-            @Override
-            public boolean hasNext() {
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return cursor != arraySize;
-            }
-
-            @Override
-            public NodeMap<K, V> next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                } else if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return array[cursor];
-            }
-        };
-    }*/
-
-
-    // Ассоциативный массив на базе хэш-таблицы должен быть унифицирован через генерики и иметь методы:
-    // boolean insert(K key, V value);
-    // V get(K key);
-    // boolean delete(K key);
-    //
-    // Реализовывать итератор.
-    // Внутренняя реализация должна использовать массив.
-    // Нужно обеспечить фиксированное время вставки и получение.
-    // Предусмотрите возможность роста хэш-таблицы при нехватке места для нового элемента.
-    //
-    // Методы разрешения коллизий реализовывать не надо.
-    // Например: если при добавлении ключ уже есть, то возвращать false.
-    //
-    // Метод hasNext проверяет, если ли следующий элемент.
-    // Многократный вызов этого метода должен быть одинаковым.
-    //
-    // Метод next возвращает первый элемент ячейки. Второй вызов метода next вернет второй элемент и так далее.
-    // Метод next сдвигает указатель итератора. Указатель - это ссылка на элемент, который нужно вернуть.
 }
--- src/test/java/ru/job4j/map/SimpleHashMapTest.java
+++ src/test/java/ru/job4j/map/SimpleHashMapTest.java
@@ -4,15 +4,60 @@ import org.junit.Test;
 
 import java.util.Iterator;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SimpleHashMapTest {
 
     @Test
-    public void insert() {
+    public void insertGet() {
         SimpleHashMap<Integer, String> shm = new SimpleHashMap<>();
         shm.insert(1112,"one");
         shm.insert(1113, "two");
-        Iterator<Integer> itr = shm.iterator();
+        assertThat(shm.get(1112), is("one"));
+        assertThat(shm.get(1113), is("two"));
+    }
+
+    @Test
+    public void insertDelete() {
+        SimpleHashMap<Integer, String> shm = new SimpleHashMap<>();
+        shm.insert(1112,"one");
+        shm.insert(1113, "two");
+        assertThat(shm.delete(1112), is(true));
+        assertThat(shm.delete(2222), is(false));
+    }
+
+    @Test
+    public void insertTwoTheSame() {
+        SimpleHashMap<Integer, String> shm = new SimpleHashMap<>();
+        shm.insert(1112,"one");
+        shm.insert(1112,"some");
+        assertThat(shm.get(1112), is("one"));
+        assertThat(shm.insert(333, "Tom"), is(true));
+        assertThat(shm.insert(333, "Bill"), is(false));
+    }
+
+    @Test
+    public void testIterator() {
+        SimpleHashMap<Integer, String> shm = new SimpleHashMap<>();
+        shm.insert(1115,"one");
+        shm.insert(1119,"two");
+        Iterator<NodeMap<Integer, String>> it = shm.iterator();
+        assertTrue(it.hasNext());
+        assertThat(it.next().getValue(), is("one"));
+        assertThat(it.next().getValue(), is("two"));
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void insertResize() {
+        SimpleHashMap<Integer, String> shm = new SimpleHashMap<>();
+        int s = 1122;
+        String str = "one";
+        for (int i = 0; i < 30; i++) {
+            shm.insert(s, str);
+            s = s * 2 % 17;
+            str += i;
+        }
     }
 }

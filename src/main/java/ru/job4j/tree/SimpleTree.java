6be90ec6fa05f675a3a6c20e16314f37a810627c
ftptpf <inboxru@yandex.ru>
2021-02-08 09:52:45 +0300

    1. Создать элементарную структуру дерева [#426144] - 1

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -2,6 +2,7 @@ package ru.job4j.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -22,6 +23,19 @@ public interface SimpleTree<E> {
 
         public Node(E value) {
             this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node<?> node = (Node<?>) o;
+            return value.equals(node.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
         }
     }
 }
--- src/main/java/ru/job4j/tree/Tree.java
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -7,16 +7,36 @@ import java.util.Queue;
 public class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
 
-    Tree(final E root) {
+    public Tree(final E root) {
         this.root = new Node<>(root);
     }
 
+    /**
+     * Метод находит узел по значению parent и добавлять в него дочерний узел со значением child.
+     * В методе реализована проверка, что значения child еще нет в дереве а parent есть.
+     * Если child есть, то метод возвращает false.
+     * @param parent
+     * @param child
+     * @return
+     */
     @Override
     public boolean add(E parent, E child) {
+        Node<E> childNode = new Node<>(child);
+        Node<E> parentNode = new Node<>(parent);
+        Optional<Node<E>> eParentNode = Optional.of(parentNode);
+        parent.
+        eParentNode.fin
+
+
         boolean rsl = false;
         return rsl;
     }
 
+    /**
+     * В методе реализован алгоритм обхода в ширину.
+     * @param value
+     * @return
+     */
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();

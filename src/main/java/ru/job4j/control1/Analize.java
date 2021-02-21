c92739d3f6ade96d2605a7d6b4aecc4b4bc9af64
ftptpf <Nb49Rtdd>
2021-02-21 16:08:00 +0300

    2. Статистику по коллекции. [#426120] - id

--- src/main/java/ru/job4j/control1/Analize.java
+++ src/main/java/ru/job4j/control1/Analize.java
@@ -1,8 +1,6 @@
 package ru.job4j.control1;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Класс определения разницы между начальным состояние массива и измененным.
@@ -19,28 +17,22 @@ public class Analize {
         int change = 0; // Счетчик измененных элементов.
         int delete = 0; // Счетчик удаленных элементов.
 
-       Set<Integer> idSet = new HashSet<>(); // Общий сет всех уникальных id двух списков previous и current.
-
         for (User cUser : current) { // Берем элемент из списка измененных данных
             if (!previous.contains(cUser)) { // Если его небыло в списке начальных данных
                 add++; // Увеличиваем счетчик добавленных элементов.
             }
-            idSet.add(cUser.getId()); // Собираем в сет id списка current.
         }
 
         for (User prUser : previous) { // Берем элемент из списка начальных данных
             if (!current.contains(prUser)) { // Если его нет в списке измененных данных
                 delete++; // Увеличиваем счетчик удаленных элементов.
             }
-            idSet.add(prUser.getId()); // Собираем в сет id списка previous.
         }
 
-        for (int id : idSet) {
-            for (User prUser : previous) {
-                for (User cUser : current) {
-                    if (id == prUser.getId() && id == cUser.getId() && !prUser.getName().equals(cUser.getName())) {
+        for (User prUser : previous) {
+            for (User cUser : current) {
+                if (prUser.getId() == cUser.getId() && !prUser.getName().equals(cUser.getName())) {
                         change++; // Увеличиваем счетчик измененных элементов.
-                    }
                 }
             }
         }

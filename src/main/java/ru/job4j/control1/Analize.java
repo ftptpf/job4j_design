16b3270a3a3e14542cd836f01b71acba13f20fe2
ftptpf <Nb49Rtdd>
2021-02-22 17:45:05 +0300

    2. Статистику по коллекции. [#426120] - map

--- src/main/java/ru/job4j/control1/Analize.java
+++ src/main/java/ru/job4j/control1/Analize.java
@@ -17,26 +17,21 @@ public class Analize {
         int change = 0; // Счетчик измененных элементов.
         int delete = 0; // Счетчик удаленных элементов.
 
-        for (User cUser : current) { // Берем элемент из списка измененных данных
-            if (!previous.contains(cUser)) { // Если его небыло в списке начальных данных
-                add++; // Увеличиваем счетчик добавленных элементов.
-            }
+        Map<Integer, String>  curMap = new HashMap<>(); // создаем map
+        for (User cUser : current) { // в него переносим все элементы current списка
+            curMap.put(cUser.getId(), cUser.getName());
         }
 
-        for (User prUser : previous) { // Берем элемент из списка начальных данных
-            if (!current.contains(prUser)) { // Если его нет в списке измененных данных
-                delete++; // Увеличиваем счетчик удаленных элементов.
+        for (User prUser : previous) { // берем элемент исходного списка previous
+            // так как результат удаления по индексу можем быть null либо String (имя пользователя) - заворачиваем все в Optional
+            Optional<String> check = Optional.ofNullable(curMap.remove(prUser.getId()));
+            if (check.isEmpty()) { // если возвращается пустое значение - ранее элемент уже был удален
+                delete++; // увеличиваем счетчик уделенных элементов
+            } else if (!check.get().equals(prUser.getName())) { // если возвращаемое значение (имя пользователя) отличается в начальном и конечном списках
+                change++; // увеличиваем счетчик измененных элементов
             }
         }
-
-        for (User prUser : previous) {
-            for (User cUser : current) {
-                if (prUser.getId() == cUser.getId() && !prUser.getName().equals(cUser.getName())) {
-                        change++; // Увеличиваем счетчик измененных элементов.
-                }
-            }
-        }
-
+        add = curMap.size(); // так как мы удалили все элементы map по индексам начального списка, в map останутся только добавленные элементы
         return new Info(add, change, delete);
     }
 }

b465af1d24bafe785e295cdb96e8b245a6946e5d
ftptpf <Nb49Rtdd>
2021-02-27 12:34:20 +0300

    0.1. FileOutputStream. [#426175]

--- src/main/java/ru/job4j/inout/MatrixToFile.java
+++ src/main/java/ru/job4j/inout/MatrixToFile.java
@@ -1,40 +1,26 @@
 package ru.job4j.inout;
 
 import java.io.FileOutputStream;
-import java.util.Arrays;
 
+/**
+ * Запись таблицы умножения в файл.
+ */
 public class MatrixToFile {
-    public static int[][] multiple(int size) {
-        int[][] table = new int[size][size];
-        try (FileOutputStream out = new FileOutputStream("matrix2.txt")) {
+    public static void main(String[] args) {
+        int size = 9; // размер массива
+        int[][] table = new int[size][size]; // массив чисел
+        String  nextLine = System.lineSeparator(); // разделитель для перехода на следующую строку
+        try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
             for (int i = 0; i < table.length; i++) {
                 for (int j = 0; j < table.length; j++) {
-                    out.write(table[i][j].);
+                    table[i][j] = (i + 1) * (j + 1);
+                    out.write(Integer.toString(table[i][j]).getBytes()); // записываем значение
+                    out.write(" ".getBytes()); // записываем пробел
                 }
+                out.write(nextLine.getBytes()); // переходим на новую строку
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-/*        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                table[i][j] = (1 + i) * (1 + j);
-            }
-        }*/
-        return table;
     }
-
-    public static void main(String[] args) {
-        multiple(9);
-    }
-/*
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(multiple(9)));
-        try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
-            out.write(multiple(9).toString().getBytes());
-            //out.write(multiple(9).toString().getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
-
 }

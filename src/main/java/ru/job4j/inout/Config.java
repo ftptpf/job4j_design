5097a1d99175bf0aa6d9bbba156e3dded5f33462
ftptpf <inboxru@yandex.ru>
2021-03-04 09:43:21 +0300

    1. Читаем файл конфигурации [#426181] - load

--- src/main/java/ru/job4j/inout/Config.java
+++ src/main/java/ru/job4j/inout/Config.java
@@ -2,27 +2,40 @@ package ru.job4j.inout;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
+import java.util.function.Predicate;
 
 /**
  * Читаем файл конфигурации.
  */
 public class Config {
-    private final String path;
-    private final Map<String, String> values = new HashMap<>();
+    private final String path; // Путь к файлу "конфигурации" с которого берем данные.
+    private final Map<String, String> values = new HashMap<>(); // Хранит "ключ-значение".
 
     public Config(final String path) {
         this.path = path;
     }
 
     /**
-     * Метод считывает все ключи в карту values.
+     * Метод считывает все ключи-значения в карту values.
+     * Ключ-значения в исходном файле разделены символов равно.
      * Пустые строки и комментарии - пропускаем.
      */
     public void load() {
-
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            Predicate<String> predicateStartsWith = s -> !s.startsWith("#");
+            Predicate<String> predicateIsEmpty = s -> !s.isEmpty();
+            in.lines()
+                    .filter(predicateIsEmpty)
+                    .filter(predicateStartsWith)
+                    // .flatMap(s -> values.put(Arrays.stream(Arrays.stream(s.split(":")).findFirst().get(), Arrays.stream(Arrays.stream(s.split(":"))..get())
+                    // .flatMap(s -> Arrays.stream(s.split(":")).map()).map();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -49,3 +62,8 @@ public class Config {
         System.out.println(new Config("app.properties"));
     }
 }
+
+
+// Для считывания файлов нужно использовать
+// import java.io.BufferedReader;
+// import java.io.FileReader;

5bb359a1273406ed2becb1b183038bed64cf6160
ftptpf <inboxru@yandex.ru>
2021-03-09 09:36:07 +0300

    1. Экзамен. Структуры данных и алгоритмы. [#426121] - Почта. [#184458]

--- src/main/java/ru/job4j/control1/UserEmails.java
+++ src/main/java/ru/job4j/control1/UserEmails.java
@@ -3,107 +3,35 @@ package ru.job4j.control1;
 import java.util.*;
 
 /**
- * Реализация слияния информции о пользователех.
+ * Реализация слияния информации о пользователях.
  * Если у двух пользователей есть общий email, значит это один и тот же пользователь.
  * Список объединенного пользователя содержит только уникальные email-ы.
  */
 public class UserEmails {
-    public List<UserData> mergeUser(List<UserData> userDataInfo) {
-        List<UserData> result = new ArrayList<>();
-        for (int i = 0; i < userDataInfo.size(); i++) {
 
-        }
-
-
-        return result;
-    }
     public Map<String, Set<String>> merge(Map<String, Set<String>> users) {
-        Map<String, Set<String>> result = new HashMap<>(users); // итоговые данные
-        //Map<String, Set<String>> tempMap = new HashMap<>(); // промежуточные данные
-        for (Map.Entry<String, Set<String>> user : users.entrySet()) {
-            String nameInputUser = user.getKey();
-            Set<String> emailsInputUsers = user.getValue();
-            for (Map.Entry<String, Set<String>> resultUser : result.entrySet()) {
-                String nameResultUser = resultUser.getKey();
-                Set<String> emailsResultUser = resultUser.getValue();
-                if (nameResultUser.equals(nameInputUser)) {
-                    users.remove(nameInputUser);
-                    continue;
-                }
-                if (!Collections.disjoint(emailsResultUser, emailsInputUsers)) {
-                    emailsResultUser.addAll(emailsInputUsers);
-                    result.put(nameResultUser, emailsResultUser);
-                    users.remove(nameInputUser);
-                }
-                 {
+        Map<String, Set<String>> result = new HashMap<>(users); // создаем файл итоговых данных помещая в него исходные данные
+        for (Map.Entry<String, Set<String>> userOne : users.entrySet()) { // берем пользователя из map
+            String nameOneUser = userOne.getKey(); // имя пользователя
+            Set<String> emailsOneUsers = userOne.getValue(); // set с email-ми
 
+            for (Map.Entry<String, Set<String>> otherUser : users.entrySet()) { // берем пользователя из map
+                String nameOtherUser = otherUser.getKey(); // имя пользователя
+                Set<String> emailsOtherUser = otherUser.getValue(); // set с email-ми
+                if (nameOneUser.equals(nameOtherUser)) { // если два имени равные
+                    continue; // прерываем внутренний цикл и берем следующего пользователя nameOtherUser
+                }
+                if (!result.containsKey(nameOneUser)) { // если nameOtherUser нет среди ключей итогового файла (был проверен и удален)
+                    continue; // прерываем внутренний цикл и берем следующего пользователя nameOtherUser
+                }
+
+                if (!Collections.disjoint(emailsOneUsers, emailsOtherUser)) { // если у двух set-ов есть хотя бы один общий email
+                    result.get(nameOneUser).addAll(emailsOtherUser); // к set-у email-ов в итоговом файле дописываем emailsOtherUser
+                    result.put(nameOneUser, emailsOneUsers); // перезаписываем map в итоговом файле
+                    result.remove(nameOtherUser); // удаляем в итоговом файле map в котором было совпадение email
                 }
             }
-/*            if (result.isEmpty()) {
-                result.put(user.getKey(), user.getValue());
-                users.remove(user.getKey());
-                continue;
-            }
-            //for ()*/
         }
-
-
-/*        Collections.disjoint()
-        //Set<String> tempSet = new HashSet<>();
-        for (Map.Entry<String, Set<String>> user : users.entrySet()) { // поочередно перебираем данные каждого пользователя
-            String currentUser = user.getKey();
-            Set<String> currentEmailsSet = new HashSet<>(user.getValue()); // временный Set
-            for (String email : user.getValue()) { // берем первый email из списка
-                tempSet.add(user.getKey()); // во временный Set добавляем пользователя
-                tempMap.put(email, tempSet.add(user.getKey())); // добавдяем во временный Map где ключь - email, а значение Set пользователей
-            }
-            //tempSet.clear();
-*//*            if (result.isEmpty()) {
-                result.put(user.getKey(), user.getValue());
-            }*//*
-        }
-        for (Map.Entry<String, Set<String>> user : tempMap.entrySet()) {
-            Set<String> tempSet = new HashSet<>();
-            for (String name : user.getValue()) {
-                tempSet.add(user.getKey());
-                result.put(user.getValue().stream().findFirst().get(), tempSet);
-            }
-        }*/
-
         return result;
     }
-/*    public Map<String, Set<Email>> mergeUser(Map<String, Set<Email>> userEmailsInfo) {
-        Map<String, Set<Email>> resultDataMap = new HashMap<>();
-        Map<Email, String> reversResultDataMap = new HashMap<>();
-        for (Map.Entry<String, Set<Email>> user : userEmailsInfo.entrySet()) {
-            String name = user.getKey();
-            Set<Email> emails = user.getValue();
-            for (Email email : emails) {
-                reversResultDataMap.put(email, name);
-            }
-        }
-        return resultDataMap;
-    }*/
-/*    public List<UserData> mergerUser(List<UserData> usersArrayList) {
-
-        for (UserData userData : usersArrayList) {
-
-            usersArrayList.remove(userData);
-        }
-
-        Map<String, UserData> userDataMap = new HashMap<>();
-        for (int i = 1; i < usersArrayList.size(); i++) {
-            UserData uData1 = usersArrayList.get(i - 1);
-            UserData uData2 = usersArrayList.get(i);
-            List<String> lStr1 = uData1.getEmailList();
-            List<String> lStr2 = uData2.getEmailList();
-            if (lStr1.retainAll(lStr2)) {
-
-            }
-        }
-
-        List<UserData> resultList = new ArrayList<>();
-
-        return resultList;
-    }*/
 }
--- src/test/java/ru/job4j/control1/UserEmailsTest.java
+++ src/test/java/ru/job4j/control1/UserEmailsTest.java
@@ -13,15 +13,35 @@ public class UserEmailsTest {
         @Test
     public void mergerUser() {
         Map<String, Set<String>> users = new HashMap<>();
-        users.put("user1", Set.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru"));
-        users.put("user2", Set.of("foo@gmail.com", "ups@pisem.net"));
-        users.put("user3", Set.of("xyz@pisem.net", "vasya@pupkin.com"));
-        users.put("user4", Set.of("ups@pisem.net", "aaa@bbb.ru"));
-        users.put("user5", Set.of("xyz@pisem.net"));
+        Set<String> usersSet1 = new HashSet<>();
+        usersSet1.add("xxx@ya.ru");
+        usersSet1.add("foo@gmail.com");
+        usersSet1.add("lol@mail.ru");
+        users.put("user1", usersSet1);
+
+        Set<String> userSet2 = new HashSet<>();
+        userSet2.add("foo@gmail.com");
+        userSet2.add("ups@pisem.net");
+        users.put("user2", userSet2);
+
+        Set<String> userSet3 = new HashSet<>();
+        userSet3.add("xyz@pisem.net");
+        userSet3.add("vasya@pupkin.com");
+        users.put("user3", userSet3);
+
+        Set<String> userSet4 = new HashSet<>();
+        userSet4.add("ups@pisem.net");
+        userSet4.add("aaa@bbb.ru");
+        users.put("user4", userSet4);
+
+        Set<String> userSet5 = new HashSet<>();
+        userSet5.add("xyz@pisem.net");
+        users.put("user5", userSet5);
+
 
         Map<String, Set<String>> afterMergerUser = new HashMap<>();
-        afterMergerUser.put("user1",Set.of("xxx@ya.ru", "foo@gmail.com", "lol@mail.ru", "ups@pisem.net", "aaa@bbb.ru"));
-        afterMergerUser.put("user3", Set.of("xyz@pisem.net", "vasya@pupkin.com"));
+        afterMergerUser.put("user1",Set.of("aaa@bbb.ru", "ups@pisem.net", "lol@mail.ru", "xxx@ya.ru", "foo@gmail.com"));
+        afterMergerUser.put("user5", Set.of("vasya@pupkin.com", "xyz@pisem.net"));
 
         UserEmails uEmails = new UserEmails();
         Map<String, Set<String>> mrUsers = uEmails.merge(users);

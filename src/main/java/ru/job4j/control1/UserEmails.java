e707cfc9db21fd85ec4a0ee3ba3981305a0472cf
ftptpf <Nb49Rtdd>
2021-03-10 21:58:27 +0300

    1. Экзамен. Структуры данных и алгоритмы. [#426121] - Почта. [#184458] - key

--- src/main/java/ru/job4j/control1/UserEmails.java
+++ src/main/java/ru/job4j/control1/UserEmails.java
@@ -10,25 +10,28 @@ import java.util.*;
 public class UserEmails {
 
     public Map<String, Set<String>> merge(Map<String, Set<String>> users) {
-        Map<String, Set<String>> result = new HashMap<>(users); // создаем файл итоговых данных помещая в него исходные данные
+        Map<String, Set<String>> result = new HashMap<>(users); // создаем файл итоговых данных помещая в него map исходных данных, далее будет происходить редактирование этого файла
+
         for (Map.Entry<String, Set<String>> userOne : users.entrySet()) { // берем пользователя из map
-            String nameOneUser = userOne.getKey(); // имя пользователя
+            String nameOneUser = userOne.getKey(); // имя основного пользователя
             Set<String> emailsOneUsers = userOne.getValue(); // set с email-ми
 
             for (Map.Entry<String, Set<String>> otherUser : users.entrySet()) { // берем пользователя из map
-                String nameOtherUser = otherUser.getKey(); // имя пользователя
+                String nameOtherUser = otherUser.getKey(); // имя "вспомогательного" пользователя которого сравниваем с основным
                 Set<String> emailsOtherUser = otherUser.getValue(); // set с email-ми
-                if (nameOneUser.equals(nameOtherUser)) { // если два имени равные
-                    continue; // прерываем внутренний цикл и берем следующего пользователя nameOtherUser
+
+                if (nameOneUser.equals(nameOtherUser)) { // если имена основного и "вспомогательного" пользователя равны
+                    continue; // прерываем внутренний цикл и берем следующего "вспомогательного" пользователя nameOtherUser
                 }
-                if (!result.containsKey(nameOneUser)) { // если nameOtherUser нет среди ключей итогового файла (был проверен и удален)
-                    continue; // прерываем внутренний цикл и берем следующего пользователя nameOtherUser
+
+                if (!result.containsKey(nameOneUser)) { // если nameOtherUser нет среди ключей итогового файла (был уже проверен и удален)
+                    continue; // прерываем внутренний цикл и берем следующего "вспомогательного" пользователя nameOtherUser
                 }
 
                 if (!Collections.disjoint(emailsOneUsers, emailsOtherUser)) { // если у двух set-ов есть хотя бы один общий email
-                    result.get(nameOneUser).addAll(emailsOtherUser); // к set-у email-ов в итоговом файле дописываем emailsOtherUser
+                    emailsOneUsers.addAll(emailsOtherUser); // к set-у email-ов основного пользователя добавляем set email-ов "вспомогательного" пользователя emailsOtherUser
                     result.put(nameOneUser, emailsOneUsers); // перезаписываем map в итоговом файле
-                    result.remove(nameOtherUser); // удаляем в итоговом файле map в котором было совпадение email
+                    result.remove(nameOtherUser); // удаляем в итоговом файле map в котором находился "вспомогательный" пользователь, в set-е которого было совпадение email c основным
                 }
             }
         }

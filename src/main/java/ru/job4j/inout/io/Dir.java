2f3da075e31a8e6dcc6550e82d7be6bf56df7119
ftptpf <Nb49Rtdd>
2021-03-14 18:18:10 +0300

    4.0. File [#426178]

---
+++ src/main/java/ru/job4j/inout/io/Dir.java
@@ -0,0 +1,22 @@
+package ru.job4j.inout.io;
+
+import java.io.File;
+
+/**
+ * Получение всех элементов директории.
+ */
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) { // проверяем существование файла
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) { // проверяем что файл это директория
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subFile : file.listFiles()) { // получаем список файлов в этой директории
+            System.out.println(subFile.getAbsoluteFile());
+        }
+    }
+}

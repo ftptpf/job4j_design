ab875c00b4704b28663892ee166f1f125463007b
ftptpf <Nb49Rtdd>
2021-03-14 18:07:40 +0300

    3.0. Тестирование IO [#426173]

--- src/test/java/ru/job4j/inout/io/AnalizyTest.java
+++ src/test/java/ru/job4j/inout/io/AnalizyTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.inout.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 
@@ -8,15 +10,17 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void unavailable() throws IOException {
-        File target = new File("resources/servernotwork.txt");
+        File target = folder.newFile("servernotwork.txt");
         StringBuilder resultFromFile = new StringBuilder();
 
         Analizy analizy = new Analizy();
         analizy.unavailable("resources/serverlog.txt");
-        analizy.writeToFile("resources/servernotwork.txt");
+        analizy.writeToFile(target.getAbsolutePath());
 
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(resultFromFile::append);

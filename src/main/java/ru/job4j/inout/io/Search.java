f556eba51a07c3ddaa188c8a5f3f830cee00c982
ftptpf <inboxru@yandex.ru>
2021-03-15 08:50:32 +0300

    4.1. Сканирование файловой системы. [#426172]

---
+++ src/main/java/ru/job4j/inout/io/Search.java
@@ -0,0 +1,15 @@
+package ru.job4j.inout.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        Files.walkFileTree(start, new PrintFiles());
+    }
+}

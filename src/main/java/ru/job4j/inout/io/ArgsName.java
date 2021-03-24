0837fa57277933cfac052561a72da26105ba2242
ftptpf <inboxru@yandex.ru>
2021-03-24 09:25:00 +0300

    5.1. Именованные аргументы [#426179] - parse

--- src/main/java/ru/job4j/inout/io/ArgsName.java
+++ src/main/java/ru/job4j/inout/io/ArgsName.java
@@ -1,10 +1,7 @@
 package ru.job4j.inout.io;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * Принимаем массив параметров и разбиваем их на пары: ключ, значение.
@@ -18,9 +15,23 @@ public class ArgsName {
 
     private void parse(String[] args) {
         /* TODO parse args to values. */
-        for (String str : args) {
-            Map<String, String> map = Arrays.stream(str.split("=", 2))
-                    .collect(Collectors.toMap(s -> s[0], s -> s[1]));
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Check the source data. It's empty.");
+        }
+        for (String str : args) { // берем строку из полученного массива строк
+            String[] keyAndParamArray = str.split("=", 2); // делим на две части по знаку "=", получаем новый массив строк
+            if (keyAndParamArray[0].startsWith("-")) { // если правая часть начинается со знака "-"
+                char[] chars = keyAndParamArray[0].toCharArray(); // переводим её в массив символов
+                char[] result = new char[chars.length - 1]; // создаем новый массив символов куда запишем результат, размер уменьшам на 1
+                int index = 0; // задаем индекс по которому у нас находится символ "-"
+                System.arraycopy(chars, index + 1, result, index, chars.length - index -1); // копируем старый массив в новый со смещением вправо отбрасывая "-"
+                keyAndParamArray[0] = new String(result); // перезаписываем значение преобразуя новый массив символов в строку
+            }
+
+            if (keyAndParamArray.length != 2 || keyAndParamArray[0].isEmpty() || keyAndParamArray[1].isEmpty()) {
+                throw new IllegalArgumentException("Check the source data. Wrong keys and parameters in it.");
+            }
+            values.put(keyAndParamArray[0],keyAndParamArray[1]);
         }
     }
 
--- src/test/java/ru/job4j/inout/io/ArgsNameTest.java
+++ src/test/java/ru/job4j/inout/io/ArgsNameTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+
 public class ArgsNameTest {
 
     @Test

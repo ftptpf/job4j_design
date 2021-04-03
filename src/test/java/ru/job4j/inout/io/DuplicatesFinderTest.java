b7ce3ab3c79eae13854f74599fbca7b86301ec56
ftptpf <Nb49Rtdd>
2021-04-03 11:30:40 +0300

    5.2. Архивировать проект [#426183] - toAbsolutePath()

--- src/test/java/ru/job4j/inout/io/DuplicatesFinderTest.java
+++ src/test/java/ru/job4j/inout/io/DuplicatesFinderTest.java
@@ -14,8 +14,12 @@ public class DuplicatesFinderTest {
     @Test
     public void main() throws IOException {
         Path start = Paths.get("resources");
-        String resultStr =
-                "{FileProperty{name='temp.txt', size=53}=[C:\\projects\\job4j_design\\resources\\1\\temp.txt, C:\\projects\\job4j_design\\resources\\2\\temp.txt]}";
+        Path one = Path.of("resources", "1", "temp.txt");
+        Path two = Path.of("resources", "2", "temp.txt");
+        /*String resultStr =
+                "{FileProperty{name='temp.txt', size=53}=[C:\\projects\\job4j_design\\resources\\1\\temp.txt, C:\\projects\\job4j_design\\resources\\2\\temp.txt]}";*/
+        String resultStr = "{FileProperty{name='temp.txt', size=53}=[" + one.toAbsolutePath().toString() + ", " + two.toAbsolutePath().toString() +"]}";
+
 
         String str = DuplicatesFinder.findDuplicate(start).toString();
         assertThat(str, is(resultStr));

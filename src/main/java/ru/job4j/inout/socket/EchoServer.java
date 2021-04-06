9300e3d354cfaa6a6119f63073e7373fbf699eff
ftptpf <inboxru@yandex.ru>
2021-04-06 08:43:03 +0300

    0. Что такое Socket? [#426197] - closeServerWork

--- src/main/java/ru/job4j/inout/socket/EchoServer.java
+++ src/main/java/ru/job4j/inout/socket/EchoServer.java
@@ -7,25 +7,36 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Сервер.
+ * При отправке сообщения клиентом: http://localhost:9000/?msg=Hello
+ * Сервер отвечает:HTTP/1.1 200 OK
+ * При отправке сообщения клиентом: http://localhost:9000/?msg=Bye
+ * Сервер отвечает:HTTP/1.1 200 OK
+ * и завершает работу.
+ */
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) { // создаем сервер по умолчанию localhost порт 9000
+            boolean closeServerWork = false; // признак закрытия сервера
             while (!server.isClosed()) { // до тех пор пока сервер не закрыт
                 Socket socket = server.accept(); // переводим сервер в режим ожидания когда к нему обратиться клиент
-                try (OutputStream out = socket.getOutputStream(); // создает поток от сервера
+                try (OutputStream out = socket.getOutputStream(); // создает поток который будут отправлять ответы от сервера
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) { // создаем поток на чтение данных от клиента
                     String str;
                     while (!(str = in.readLine()).isEmpty()) { // все прочитанные данные от клиента
                         System.out.println(str); // выводим на консоль
                         if (str.startsWith("GET") && str.contains("Bye")) { // если строка ответа начинается с "GET" и содержит "Bye"
-                            out.write("HTTP/1.1 200 OK".getBytes()); // записываем ответ - "HTTP/1.1 200 OK"
-                            out.write(System.lineSeparator().getBytes()); // записываем перевод строки
-                            server.close(); // закрываем сервер
+                            closeServerWork = true;
                         }
                     }
                     out.write("HTTP/1.1 200 OK".getBytes()); // записываем ответ - "HTTP/1.1 200 OK"
                     out.write(System.lineSeparator().getBytes()); // записываем перевод строки
+                    if (closeServerWork) { // делаем проверку требуется ли закрыть сервер
+                        server.close(); // закрываем сервер
+                        closeServerWork = false;
+                    }
                 }
             }
         }

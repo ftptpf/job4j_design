d80bc04f8d51940b2a1851fdcb9eedf1f3447e26
ftptpf <Nb49Rtdd>
2021-04-11 19:43:35 +0300

    3. Slf4j - вывод переменных. [#426169]

---
+++ src/main/java/ru/job4j/inout/logging/UsageLog4jVariables.java
@@ -0,0 +1,26 @@
+package ru.job4j.inout.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class UsageLog4jVariables {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4jVariables.class.getName());
+
+    public static void main(String[] args) {
+        String name = "Ivan Ivanov";
+        int age = 33;
+        String city = "Krasnoyarsk";
+        String street = "Lenina str.";
+        int flat = 12;
+        int passport = 896656562;
+        long distance = 333243423333332334L;
+        char ch = 80;
+        double p = 3.14;
+        LOG.debug("User info name: {}, age: {} ", name, age);
+        LOG.debug("Address: {} , {}, {}", city, street, flat);
+        LOG.debug("Passport: {}", passport);
+        LOG.debug("Distance from NY to Krasnoyarsk: {} miles", distance);
+        LOG.debug("The: {} is equals to {}" , ch, p);
+    }
+}

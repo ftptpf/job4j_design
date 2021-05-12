15548e2ebd9dc233032e9de0f44f4b58eeb56178
ftptpf <inboxru@yandex.ru>
2021-05-12 09:56:12 +0300

    1. Симуляция терминала Linux - parseAbsolute

--- src/main/java/ru/job4j/inout/exam/Shell.java
+++ src/main/java/ru/job4j/inout/exam/Shell.java
@@ -3,38 +3,58 @@ package ru.job4j.inout.exam;
 import java.util.*;
 
 public class Shell {
-    String result;
-    List<String> list = new ArrayList<>();
+    private static String result;
+    private List<String> list = new ArrayList<>();
+    private static Deque<String> stack = new ArrayDeque<>();
+
+    /**
+     * Изменяет текущую директорию.
+     * @param path
+     */
     public void cd(String path) {
         if (path.startsWith("/")) {
             parseAbsolute(path);
         } else {
             parseRelative(path);
         }
-        //list.add(path);
     }
+
+    /**
+     * Выводим в терминале путь к текущей папке.
+     * @return
+     */
     public String pwd() {
-/*        int lastIndex = list.size() - 1;
-        String last = list.get(lastIndex);
-        if (last.equals("..") || last.equals("/")) {
-            result = "/";
-        } else if (last.equals("../root")) {
-            result = "/root";
-        } else if (list.size() == 2 && (list.get(0).startsWith("/") && list.get(1).startsWith("/"))) {
-            result = list.get(1);
-        } else if (list.size() == 2 && !list.contains("/")) {
-            result = "/" + list.get(0) + "/" + list.get(1);
-        }*/
         return result;
     }
 
+    /**
+     * Получаем абсолютный путь, разбиваем по / и заново все помещаем в стек,
+     * предварительно его опустошая.
+     * @param path
+     */
     public static void parseAbsolute(String path) {
-        String[] array = path.split("/");
-        Deque<String> stack = new ArrayDeque<>();
-
-
+        String[] array = path.split("/"); // из абсолютного пути создаем массив директорий
+        if (array.length == 0) { // если массив пуст
+            result = "/"; // результату присваиваем корневую директорию
+        } else {
+            for (String dir: array) {
+                if (!dir.isEmpty()) { // отфильтровываем пустые названия
+                    stack.addFirst(dir); // размещаем названия директорий в начало стека
+                }
+            }
+            StringBuilder sb = new StringBuilder("");
+            while (!stack.isEmpty()) { // пока стек содержит данные
+                sb.append("/").append(stack.removeLast()); // извлекаем из конца стека(очередь) имена директорий и собираем в абсолютный путь
+            }
+            result = sb.toString();
+        }
     }
 
+    /**
+     * Получаем относительный путь (.. либо название директории).
+     * Здесь уже манипулируете стеком.
+     * @param path
+     */
     public static void parseRelative(String path) {
     }
 }
--- src/test/java/ru/job4j/inout/exam/ShellTest.java
+++ src/test/java/ru/job4j/inout/exam/ShellTest.java
@@ -10,8 +10,8 @@ public class ShellTest {
     @Test
     public void whenCdBack() {
         Shell shell = new Shell();
-        shell.cd("/user");
-        shell.cd("../root");
+        shell.cd("/user"); // указываем путь к директории
+        shell.cd("../root"); // переходим в родительский каталог и из него в папку root
         assertThat(
                 shell.pwd(), is("/root")
         );
@@ -20,15 +20,15 @@ public class ShellTest {
     @Test
     public void whenAbsolutePath() {
         Shell shell = new Shell();
-        shell.cd("/path/to/file");
-        shell.cd("/new/path/to/my/file");
+        shell.cd("/path/to/file"); // указываем путь к директории
+        shell.cd("/new/path/to/my/file"); // явно указываем новый путь к директории
         assertThat(shell.pwd(), is("/new/path/to/my/file"));
     }
 
     @Test
     public void whenCdRoot() {
         Shell shell = new Shell();
-        shell.cd("/");
+        shell.cd("/"); // указываем путь к корневой директории
         assertThat(
                 shell.pwd(), is("/")
         );
@@ -37,8 +37,8 @@ public class ShellTest {
     @Test
     public void whenCdUserLocal() {
         Shell shell = new Shell();
-        shell.cd("user");
-        shell.cd("local");
+        shell.cd("user"); // указываем папку, по факту /user
+        shell.cd("local"); // переходим в папку local, которая хранится в user
         assertThat(
                 shell.pwd(), is("/user/local")
         );
@@ -48,7 +48,7 @@ public class ShellTest {
     public void whenCdUserBack() {
         Shell shell = new Shell();
         shell.cd("user");
-        shell.cd("..");
+        shell.cd(".."); // переходим в родительский каталог
         assertThat(
                 shell.pwd(), is("/")
         );

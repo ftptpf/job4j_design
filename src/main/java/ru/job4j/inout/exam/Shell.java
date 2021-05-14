aa131b47fd026935b0e711934ffc7533a9434e12
ftptpf <Nb49Rtdd>
2021-05-14 22:43:33 +0300

    1. Экзамен. Ввод-вывод. [#426193] - 1. Симуляция терминала Linux - takeFromQueue()

--- src/main/java/ru/job4j/inout/exam/Shell.java
+++ src/main/java/ru/job4j/inout/exam/Shell.java
@@ -1,18 +1,16 @@
 package ru.job4j.inout.exam;
 
-import java.nio.file.Path;
 import java.util.*;
 
 /**
  * Симуляция работы в терминале Linux команд "cd" и "pwd".
  */
 public class Shell {
-    private String result;
-    // private List<String> list = new ArrayList<>();
-    private Deque<String> queue = new ArrayDeque<>();
+    private String result = "";
+    private Deque<String> queue = new ArrayDeque<>(); // очередь в которую будут собираться и извлекаться наименования директорий
 
     /**
-     * Симуляция работы команды смены диретории.
+     * Симуляция работы команды смены директории.
      * @param path
      */
     public void cd(String path) {
@@ -32,8 +30,7 @@ public class Shell {
     }
 
     /**
-     * Получаем абсолютный путь, разбиваем по / и заново все помещаем в стек,
-     * предварительно его опустошая.
+     * Работаем с даными абсолютного пути.
      * @param path
      */
     public void parseAbsolute(String path) {
@@ -47,29 +44,29 @@ public class Shell {
     }
 
     /**
-     * Получаем относительный путь (.. либо название директории).
-     * Здесь уже манипулируете стеком.
+     * Работаем с данного относительного пути.
      * @param path
      */
     public void parseRelative(String path) {
-        String[] array = path.split("/"); // из абсолютного пути создаем массив директорий
-        if (array.length > 0 && array[0].equals("..")) {
-            // array[0] = "";
-            String[] oldArray;
-            if (!result.isEmpty()) {
-                oldArray = result.split("/");
-            } else {
-                oldArray = new String[]{""};
-            }
-
+        String[] array = path.split("/"); // из нового абсолютного пути создаем массив директорий
+        String[] oldArray = result.split("/"); // из текущего абсолютного пути создаем массив директорий
+        if (array.length > 0 && !array[0].equals("..")) {
             addToQueue(oldArray);
-            queue.removeLast();
             addToQueue(array);
-            queue.removeLast();
-            takeFromQueue();
+        } else {
+            addToQueue(oldArray); // добавляем в очередь текущий массив
+            queue.removeFirst(); // из него убираем первое значение, т.е. перемещаемся в родительскую директорию
+            addToQueue(array); // добавляем новый массив директорий
+            queue.removeLast(); // убираем последнее значение, т.е. перемещаемся во вложенную папку
         }
+        takeFromQueue();
+
     }
 
+    /**
+     * Добавляем массив директорий в очередь.
+     * @param array
+     */
     public void addToQueue(String[] array) {
         for (String dir: array) {
             if (!dir.isEmpty()) { // отфильтровываем пустые названия
@@ -78,14 +75,18 @@ public class Shell {
         }
     }
 
+    /**
+     * Извлекаем из очереди абсолютный путь.
+     * @return
+     */
     public String takeFromQueue() {
         StringBuilder sb = new StringBuilder();
-        while (!queue.isEmpty()) { // пока стек содержит данные
+        if (queue.isEmpty()) { // если очередь пуста
+            sb = sb.append("/"); // присваиваем значение корневой директории
+        }
+        while (!queue.isEmpty()) { // пока очередь содержит данные
             sb.append("/").append(queue.removeLast()); // извлекаем из конца очереди имена директорий и собираем в абсолютный путь
         }
         return result = sb.toString();
     }
-
-
-
 }

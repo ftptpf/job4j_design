45e12a41aca97d8376288036efb3b6539fa2d080
ftptpf <Nb49Rtdd>
2021-05-19 22:45:34 +0300

    0. JDBC [#6863]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -4,8 +4,16 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
 
 public class ConnectionDemo {
+
+    Properties properties = new Properties();
+
+
+
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/idea_db";

86b586d99c00b67ebd79b34360dabb77eb9ec07b
ftptpf <inboxru@yandex.ru>
2021-05-30 10:00:39 +0300

    Поиск файлов по критерию [#783 #42852] - Predicate<Path> getSearchCondition(String type, String name)

--- src/main/java/ru/job4j/inout/control/FileFind.java
+++ src/main/java/ru/job4j/inout/control/FileFind.java
@@ -36,7 +36,10 @@ public class FileFind extends SimpleFileVisitor<Path> {
      */
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (t.equals("name") && n.equals(file.toFile().getName())) {
+        if (getSearchCondition(t, n).test(file)) {
+            resultListPath.add(file);
+        }
+/*        if (t.equals("name") && n.equals(file.toFile().getName())) {
             resultListPath.add(file);
         } else if (t.equals("mask")) {
             String regex = n
@@ -50,10 +53,17 @@ public class FileFind extends SimpleFileVisitor<Path> {
             if (Pattern.matches(n, file.toFile().getName())) {
                 resultListPath.add(file);
             }
-        }
+        }*/
         return CONTINUE;
     }
-    public static Predicate<Path> getSearchCondition(String mask) {
+
+    /**
+     * Условие отбора фалов.
+     * @param type тип поиска: mask искать по маске, name по полному совпадение имени, regex по регулярному выражению.
+     * @param name имя файла, маска, либо регулярное выражение.
+     * @return
+     */
+    public static Predicate<Path> getSearchCondition(String type, String name) {
         Predicate<Path> pathPredicate = new Predicate<Path>() {
             @Override
             public boolean test(Path path) {

83b53bb868e4d85cfa575e4615352279a1e6f84f
ftptpf <inboxru@yandex.ru>
2021-06-01 09:54:04 +0300

    3. Загрузка базы спамеров. [#20459] - stream

--- src/main/java/ru/job4j/jdbc/spammer/ImportDB.java
+++ src/main/java/ru/job4j/jdbc/spammer/ImportDB.java
@@ -6,27 +6,43 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * Из текстового файла переносим данные в postgreSQL.
+ */
 public class ImportDB {
-    private Properties cfg;
-    private String dump;
+    private Properties cfg; // файл где хранятся параметры базы данных
+    private String dump; // текстовый файл который мы парсим
 
     public ImportDB(Properties cfg, String dump) {
         this.cfg = cfg;
         this.dump = dump;
     }
 
+    /**
+     * Считываем данные с текстового файла.
+     * @return
+     * @throws IOException
+     */
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            /*rd.lines().forEach(...); */
+            String[] strings = rd.lines().forEach(str -> str.split(";", 2)));
+            // rd.lines().forEach(str -> Arrays.stream(str.split(";")).collect());
 
         }
         return users;
     }
 
+    /**
+     * Инициализируем соедениение с базой данных PostgreSQL и сохраняем в нее ранее считанные данный.
+     * @param users
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     public void save(List<User> users) throws ClassNotFoundException, SQLException {
         Class.forName(cfg.getProperty("jdbc.driver"));
         try (Connection cnt = DriverManager.getConnection(
@@ -35,21 +51,40 @@ public class ImportDB {
                 cfg.getProperty("jdbc.password")
         )) {
             for (User user : users) {
-                try (PreparedStatement ps = cnt.prepareStatement("insert info users ...")) {
-                    ps.setString(1, user.name);
-                    ps.setString(1, user.email);
+                try (PreparedStatement ps = cnt.prepareStatement("INSERT INTO spam_db(name, email) VALUES(? , ?)")) {
+                    ps.setString(1, user.getName());
+                    ps.setString(2, user.getEmail());
                     ps.execute();
                 }
             }
         }
     }
 
+    /**
+     * Класс структуры данных пользователей.
+     */
     private static class User {
         String name;
         String email;
 
         public User(String name, String email) {
             this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
             this.email = email;
         }
     }

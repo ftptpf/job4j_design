9147a320d9392a499260314f8c9cb8d1df0cb2c0
ftptpf <inboxru@yandex.ru>
2021-06-02 09:40:46 +0300

    3. Загрузка базы спамеров. [#20459] - stream add

--- src/main/java/ru/job4j/jdbc/spammer/ImportDB.java
+++ src/main/java/ru/job4j/jdbc/spammer/ImportDB.java
@@ -5,10 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Из текстового файла переносим данные в postgreSQL.
@@ -30,8 +28,21 @@ public class ImportDB {
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            String[] strings = rd.lines().forEach(str -> str.split(";", 2)));
-            // rd.lines().forEach(str -> Arrays.stream(str.split(";")).collect());
+            Map<String, String> map = new HashMap<>();
+            String[] array  = rd.lines()
+                    .forEach(str -> Arrays.stream(str.split(";", 2))
+                            .collect(Collectors.flatMapping(s -> s[0], )));
+                    //.map(str -> str.split(";", 2))
+                            //.collect(Collectors.toMap(s -> s[0], s -> s[1]));
+            //String[] strings = rd.lines().forEach(str -> str.split(";", 2)));
+            //rd.lines().forEach(str -> Arrays.stream(str.split(";")).collect(Collectors.toList()));
+/*
+            rd.lines()
+                    .forEach(
+                            str -> Arrays.stream(
+                                    str.split(";", 2))
+                                    .map(Collectors.toList(new User())));
+*/
 
         }
         return users;
@@ -97,6 +108,4 @@ public class ImportDB {
         ImportDB db = new ImportDB(cfg, "resources/spam/dump.txt");
         db.save(db.load());
     }
-
-
 }

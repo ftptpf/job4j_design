4de2b69ba14931a3ba1885022fc745e19d99cc9b
ftptpf <inboxru@yandex.ru>
2021-08-24 09:14:48 +0300

    1. Принципы Kiss, Dry и Yagni [#238813] - BiPredicate

--- src/main/java/ru/job4j/ood/tdd/MaxMin.java
+++ src/main/java/ru/job4j/ood/tdd/MaxMin.java
@@ -1,27 +1,25 @@
 package ru.job4j.ood.tdd;
 
 import java.util.*;
+import java.util.function.BiPredicate;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        Iterator<T> iterator = value.iterator();
-        T result = iterator.next();
-        while (iterator.hasNext()) {
-            T next = iterator.next();
-            if (comparator.compare(next, result) > 0) {
-                result = next;
-            }
-        }
-
-        return result;
+        BiPredicate<T, T> biPredicate = (file1, file2) -> comparator.compare(file1, file2) < 0;
+        return find(biPredicate, value);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
+        BiPredicate<T, T> biPredicate = (file1, file2) -> comparator.compare(file1, file2) > 0;
+        return find(biPredicate, value);
+    }
+
+    static <T> T find(BiPredicate<T, T> biPredicate, List<T> value) {
         Iterator<T> iterator = value.iterator();
         T result = iterator.next();
         while (iterator.hasNext()) {
             T next = iterator.next();
-            if (comparator.compare(next, result) < 0) {
+            if (biPredicate.test(result, next)) {
                 result = next;
             }
         }

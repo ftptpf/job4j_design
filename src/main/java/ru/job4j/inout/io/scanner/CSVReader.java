116dfa6cef83d8e03dd015167b1ec9ada050d193
ftptpf <inboxru@yandex.ru>
2021-10-14 09:49:55 +0300

    7. Scanner [#504791]

---
+++ src/main/java/ru/job4j/inout/io/scanner/CSVReader.java
@@ -0,0 +1,9 @@
+package ru.job4j.inout.io.scanner;
+
+import ru.job4j.inout.io.namedarguments.ArgsName;
+
+public class CSVReader {
+    public static void handle(ArgsName argsName) throws Exception {
+
+    }
+}

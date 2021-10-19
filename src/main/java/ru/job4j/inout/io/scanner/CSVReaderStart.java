78f1e21ebab7d6428d0c3d260d4f5995667340ef
ftptpf <inboxru@yandex.ru>
2021-10-19 09:18:59 +0300

    7. Scanner [#504791] - CSVReaderStart

---
+++ src/main/java/ru/job4j/inout/io/scanner/CSVReaderStart.java
@@ -0,0 +1,14 @@
+package ru.job4j.inout.io.scanner;
+
+import ru.job4j.inout.io.namedarguments.ArgsName;
+
+/**
+ * Запуск класса CSVReader с аргументами, которые мы получаем из командной строки.
+ */
+public class CSVReaderStart {
+
+    public static void main(String[] args) throws Exception {
+        ArgsName argsName = ArgsName.of(args);
+        CSVReader.handle(argsName);
+    }
+}
